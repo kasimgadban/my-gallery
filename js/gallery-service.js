5f0fe335dd03ff20cf33696926fa6dfd1ee87f51
kasim <kasim.gadban@gmail.com>
2018-10-15 12:36:02 +0300

    rename book shop porj to book-shop

--- js/gallery-service.js
+++ js/gallery-service.js
@@ -5,7 +5,7 @@ var gDate = new Date();
 
 function creatProjects(){
     gProj = [
-        creatProject('book shop','book shop','pick your best book','online old books shop','projs/book shop/index.html'),
+        creatProject('book shop','book shop','pick your best book','online old books shop','projs/book-shop/index.html'),
         creatProject('chess','chess','Lets play chess!','chess board game','projs/mister-chess/index.html'),
         creatProject('guess who','guess who','Think of someone..','Let me guess who you think about','projs/guess-who/index.html'),
         creatProject('in picture','in picture','Whats in the picture?','Click the sentence that describes the picture','projs/in-picture/index.html'),
